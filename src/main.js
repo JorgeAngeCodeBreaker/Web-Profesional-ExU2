4fba757dd8b6b91db79d4e179f9b1b5f5ea3d30d
EmilianoChofis <20213tn128@utez.edu.mx>
2024-02-27 20:01:51 -0600

    carouselPruebas1

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+// main.js
 import Vue from 'vue'
 import App from './App.vue'
+import 'jquery'; // Importa jQuery antes que Bootstrap
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './assets/main.css'
 
 new Vue({
   render: (h) => h(App)
